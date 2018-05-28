19b99011d68553dc7f3e7af0ec6472998197afd5
@Max <ritcma2020@SICT-9.local>
2018-05-28 12:57:05 +0800

    XML Stage 1

---
+++ STEM Comp 2018/Assets/Scripts/RobotData.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Xml.Serialization;
+
+public class RobotData
+{
+	public List<BlockData> blocks;
+
+	public static void Save (string filepath, RobotData rd)
+	{
+		using (FileStream fs = new FileStream (filepath, FileMode.Create)) {
+			rd.blocks = BlockData.PrepareBlockData ();
+			XmlSerializer xml = new XmlSerializer (typeof(RobotData));
+			xml.Serialize (fs, rd);
+		}
+	}
+
+	public static RobotData LoadFromFile (string filepath) {
+		using (FileStream fs = new FileStream (filepath, FileMode.Open)) {
+			XmlSerializer xml = new XmlSerializer (typeof(RobotData));
+			return (RobotData)xml.Deserialize (fs);
+		}
+	}
+}
+
+public class BlockData
+{
+	public Vector3 position;
+	public Vector3 rotation;
+
+	[XmlAttribute]
+	public string type;
+
+	[XmlArray ("State List")]
+	[XmlArrayItem ("state")]
+	public List<State> states;
+
+	public static List<BlockData> PrepareBlockData () {
+		List<BlockData> blockDataList = new List<BlockData> ();
+		foreach (Block block in Block.magicListOfAllBlocks) {
+			if (block.attached) {
+				BlockData bd = new BlockData ();
+				bd.position = block.transform.position;
+				bd.rotation = block.desiredRotation;
+				bd.type = block.GetType ().ToString ();
+				bd.states = block.states;
+				blockDataList.Add (bd);
+			}
+		}
+		return blockDataList;
+	}
+}
---
+++ STEM Comp 2018/Assets/Scripts/RobotDataTester.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotDataTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void TestRobotData () {
+		RobotData rd = new RobotData ();
+		RobotData.Save (@"/Users/ritcma2020/Desktop/test.xml", rd);
+	}
+}
