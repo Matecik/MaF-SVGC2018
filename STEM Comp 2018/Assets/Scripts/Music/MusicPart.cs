6e6ea3700372112412aea80fe7fe7946e90b8bfb
AtMax <22337374+Matecik@users.noreply.github.com>
2018-07-12 11:54:55 +0800

    ONE SHOT

---
+++ STEM Comp 2018/Assets/Scripts/Music/MusicPart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MusicPart", menuName = "Parts/MusicPart", order = 1)]
+public class MusicPart : ScriptableObject {
+
+    public string partName;
+
+    public AudioClip AudioClip;
+
+    [Range(0f,1f)]
+    public float volume;
+    [Range(0.1f,2f)]
+    public float pitch = 1f;
+
+    [HideInInspector]
+    public AudioSource source;
+
+}
